4923b9226432b4568eac4ce918de09553a359a11
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2025-04-09 11:03:19 -0500

    some new things

--- RUNall.sh
+++ RUNall.sh
@@ -8,8 +8,10 @@
 #split the HEpevt file into equal 40 events files and TAR
 ./splitHEPevt.sh
 
+#copy over the tarm unzip, script and 
 cp SplittingHEP_40evts.tar /pnfs/uboone/resilient/users/markross/tars/
 cp unzip_script_HEPevt.sh /pnfs/uboone/resilient/users/markross/scripts/
 cp HEPevt_AddOverlay_batch_TEMPLATE.fcl /pnfs/uboone/resilient/users/markross/fcls/
 
+#and then submit as you want
 project.py --xml fromHEPevt_withOverlays_v1.0.xml --stage genartroot --submit
